1ec9d990444e23bc9c8b66e7e62aaeca6970437d
peegra <36023142+peegra@users.noreply.github.com>
2026-01-27 09:50:41 +0900

    アナリティクス削除

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
